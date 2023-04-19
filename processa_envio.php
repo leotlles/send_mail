67a552e991c93e4db12faf7fba51a556060423d5
leotlles <tryagainleo@proton.me>
2023-04-19 17:52:09 -0300

    adding&config-PHPMailer

--- processa_envio.php
+++ processa_envio.php
@@ -1,5 +1,13 @@
 <?php
 
+    require "./libs/PHPMailer/Exception.php";
+    require "./libs/PHPMailer/OAuth.php";
+    require "./libs/PHPMailer/PHPMailer.php";
+    require "./libs/PHPMailer/POP3.php";
+    require "./libs/PHPMailer/SMTP.php";
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
 
     class Mensagem {
         private $para = null;
@@ -29,11 +37,45 @@ $mensagem->__set('mensagem', $_POST['mensagem']);
 
 // print_r($mensagem);
 
-if($mensagem->mensagemValida()) {
-    echo 'Mensagem válida';
-} else {
+if(!$mensagem->mensagemValida()) {
     echo 'Mensagem inválida';
+    die();
 }
 
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                     
+    $mail->isSMTP();                                          
+    $mail->Host       = 'smtp.gmail.com';                    
+    $mail->SMTPAuth   = true;                                  
+    $mail->Username   = 'username@email.com';                // ATENÇÃO 
+    $mail->Password   = 'password';                          // ATENÇÃO           
+    $mail->SMTPSecure = 'tls';           
+    $mail->Port       = 587;                                    
+
+    //Recipients
+    $mail->setFrom('from@email.com', 'Remetente');
+    $mail->addAddress('to@email.com', 'Destinatário');  
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    
+
+    //Content
+    $mail->isHTML(true);                               
+    $mail->Subject = 'Oi, eu sou o assunto';
+    $mail->Body    = 'Oi, sou conteudo do  <strong>email</strong>';
+    $mail->AltBody = 'Oi, sou conteudo do email';
+
+    $mail->send();
+    echo 'Email foi enviado com sucesso!';
+} catch (Exception $e) {
+    echo "Não foi possível enviar a mensagem, tente novamente mais tarde. Detalhes do erro: {$mail->ErrorInfo}";
+}
 
 ?>
